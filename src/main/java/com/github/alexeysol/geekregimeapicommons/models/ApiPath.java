61ec480f54e8271e2c0a0fedaf3caee5fb19fc5d
Alexey Soloviev <alexey.soloviev.w@gmail.com>
2022-08-13 19:15:40 +0300

    Add default path formatter to ApiPath

--- src/main/java/com/github/alexeysol/geekregimeapicommons/models/ApiPath.java
+++ src/main/java/com/github/alexeysol/geekregimeapicommons/models/ApiPath.java
@@ -2,4 +2,8 @@ package com.github.alexeysol.geekregimeapicommons.models;
 
 public interface ApiPath {
     String getApiPath(int version);
+
+    default String formatApiPath(String prefix, int version, String resource) {
+        return String.format("/%s/v%d/%s", prefix, version, resource);
+    }
 }
