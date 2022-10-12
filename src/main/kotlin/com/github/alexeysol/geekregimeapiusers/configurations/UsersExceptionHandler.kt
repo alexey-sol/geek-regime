2f81697b6b84b7790d892d2b73d5bc5b597ec157
Alexey Soloviev <alexey.soloviev.w@gmail.com>
2022-10-12 21:29:23 +0300

    Minor fix: make ApiUsersSource to provide profile info

--- src/main/kotlin/com/github/alexeysol/geekregimeapiusers/configurations/UsersExceptionHandler.kt
+++ src/main/kotlin/com/github/alexeysol/geekregimeapiusers/configurations/UsersExceptionHandler.kt
@@ -1,9 +1,9 @@
 package com.github.alexeysol.geekregimeapiusers.configurations
 
-import com.github.alexeysol.geekregimeapicommons.configurations.ResourceExceptionHandler
+import com.github.alexeysol.geekregimeapicommons.configurations.ApiExceptionHandler
 import com.github.alexeysol.geekregimeapiusers.utils.sources.ApiUsersSource
 import org.springframework.web.bind.annotation.ControllerAdvice
 
 @ControllerAdvice
 class UsersExceptionHandler(source: ApiUsersSource) :
-    ResourceExceptionHandler(source.resource, source.isProduction())
+    ApiExceptionHandler(source.resource, source.isProduction())
--- src/main/kotlin/com/github/alexeysol/geekregimeapiusers/utils/sources/ApiUsersSource.kt
+++ src/main/kotlin/com/github/alexeysol/geekregimeapiusers/utils/sources/ApiUsersSource.kt
@@ -1,23 +1,22 @@
 package com.github.alexeysol.geekregimeapiusers.utils.sources
 
-import com.github.alexeysol.geekregimeapicommons.models.ApiPath
+import com.github.alexeysol.geekregimeapicommons.models.sources.ActiveProfile
+import com.github.alexeysol.geekregimeapicommons.models.sources.ApiPath
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
 data class ApiUsersSource (
-    private val PRODUCTION_PROFILE: String? = "prod",
-
     @Value("\${spring.profiles.active}")
-    private val activeProfile: String? = null,
+    private val activeProfile: String,
 
     @Value("\${api-users.prefix}")
-    private val apiPrefix: String? = null,
+    private val apiPrefix: String,
 
     @Value("\${api-users.resource}")
     val resource: String? = null,
-) : ApiPath {
-    fun isProduction(): Boolean = PRODUCTION_PROFILE == activeProfile
-
+) : ApiPath, ActiveProfile {
     override fun getApiPath(version: Int): String = formatApiPath(apiPrefix, version, resource)
+
+    override fun getActiveProfile(): String = activeProfile
 }
