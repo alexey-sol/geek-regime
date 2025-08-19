9e14ceb98275888f6abbe67b62bac5c1c13abff2
Alexey Soloviev <alexey.soloviev.p@gmail.com>
2025-08-19 22:15:12 +0300

    [client-web] Add favicon, downgrade react-router-dom due to issues with params in paginated routes

--- client-web/tools/webpack/webpack.base.ts
+++ client-web/tools/webpack/webpack.base.ts
@@ -8,6 +8,9 @@ import { getRules } from "@eggziom/geek-regime-js-configs/webpack";
 const cwd = process.cwd();
 const publicDir = path.join(cwd, "src", "public");
 
+const META_DESCRIPTION = "Geek Regime: a collaborative blog";
+const META_VIEWPORT = "width=device-width, initial-scale=1";
+
 const config: CallableOption = (env, argv) => ({
     resolve: {
         alias: {
@@ -19,6 +22,11 @@ const config: CallableOption = (env, argv) => ({
             systemvars: true,
         }),
         new HtmlWebpackPlugin({
+            favicon: path.join(publicDir, "images", "favicon.ico"),
+            meta: {
+                description: META_DESCRIPTION,
+                viewport: META_VIEWPORT,
+            },
             template: path.join(publicDir, "index.html"),
         }),
     ],
